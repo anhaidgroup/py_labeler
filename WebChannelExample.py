d750ccd4ac81c80a8116f08b54bd98cfd19a0810
pavankm <pavank@protonmail.com>
2017-02-24 00:10:32 -0600

    attempt at fixing console warning

--- WebChannelExample.py
+++ WebChannelExample.py
@@ -1,10 +1,12 @@
 from PyQt5.Qt import *
+from PyQt5 import QtCore
 from OpenGL import GL  # Do not auto clean imports !! from OpenGL import GL is needed for linux
 
 # todo 2/23/17  issue seems to be with linux
 # todo 2/23/17  check error with blank screen. ref: https://riverbankcomputing.com/pipermail/pyqt/2014-January/033681.html
 
 qwebchannel_js = QFile(':/qtwebchannel/qwebchannel.js')
+# todo 2/23/17 check http://lists.qt-project.org/pipermail/qtwebengine/2016-March/000339.html about injecting js to every file
 if not qwebchannel_js.open(QIODevice.ReadOnly):
     raise SystemExit(
         'Failed to load qwebchannel.js with error: %s' %
@@ -15,9 +17,9 @@ qwebchannel_js = bytes(qwebchannel_js.readAll()).decode('utf-8')
 def client_script():
     script = QWebEngineScript()
     script.setSourceCode(qwebchannel_js + '''
-    new QWebChannel(qt.webChannelTransport, function(channel) {
-        channel.objects.bridge.respond('Hello world!');
-    });
+//    new QWebChannel(qt.webChannelTransport, function(channel) {
+//        channel.objects.bridge.respond('Hello world!');
+//    });
 
     var button = document.getElementById('hello');
     button.onclick = function(){
@@ -33,11 +35,24 @@ def client_script():
 
 
 class WebPage(QWebEnginePage):
+    # selectedText = pyqtProperty(str, notify=sigDataChanged)
+
+    def __init__(self):
+        super(WebPage, self).__init__(None)
+
+    #     self.selectionChanged.connect(self.handleSelectionChange)
+    #     self.titleChanged.connect(self.handleSelectionChange)
+
     def javaScriptConsoleMessage(self, level, msg, linenumber, source_id):
         try:
             print('%s:%s: %s' % (source_id, linenumber, msg))
         except OSError:
             pass
+
+            # @pyqtSlot(str)
+            # def handleSelectionChange(self):
+            #     return
+            # selectedText = pyqtProperty(str, notify=sigDataChanged)
 
     @pyqtSlot(str)
     def respond(self, text):
