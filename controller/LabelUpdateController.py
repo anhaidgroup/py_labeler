fe93a63c78da00dde0abd6c92fa9853cdc394206
pavankm <pavank@protonmail.com>
2017-05-04 19:09:29 -0500

    refresh page on change label

--- controller/LabelUpdateController.py
+++ controller/LabelUpdateController.py
@@ -12,15 +12,31 @@ class LabelUpdateController(QObject):
     @pyqtSlot(str, str)
     def change_label(self, tuple_pair_id, new_label):
         ApplicationContext.COMPLETE_DATA_FRAME.loc[ApplicationContext.COMPLETE_DATA_FRAME['_id'] == int(tuple_pair_id), 'label'] = new_label
+        self.main_page.setHtml(
+            Renderer.render_main_page(
+                current_page_tuple_pairs=
+                ApplicationContext.PAGINATION_CONTROLLER.get_tuples_for_page(ApplicationContext.current_page_number),
+                match_count=
+                ApplicationContext.current_data_frame[ApplicationContext.current_data_frame.label == ApplicationContext.MATCH].shape[0],
+                not_match_count=
+                ApplicationContext.current_data_frame[ApplicationContext.current_data_frame.label == ApplicationContext.NON_MATCH].shape[0],
+                not_sure_count=
+                ApplicationContext.current_data_frame[ApplicationContext.current_data_frame.label == ApplicationContext.NOT_SURE].shape[0],
+                unlabeled_count=
+                ApplicationContext.current_data_frame[ApplicationContext.current_data_frame.label == ApplicationContext.NOT_LABELED].shape[0],
+            )
+        )
 
     @pyqtSlot(str, result=str)
     def show_tuple_pair(self, tuple_pair_id):
-        string = Renderer.render_tuple_pair(ApplicationContext.COMPLETE_DATA_FRAME.loc[ApplicationContext.COMPLETE_DATA_FRAME['_id'] == int(tuple_pair_id)])
+        string = Renderer.render_tuple_pair(
+            ApplicationContext.COMPLETE_DATA_FRAME.loc[ApplicationContext.COMPLETE_DATA_FRAME['_id'] == int(tuple_pair_id)])
         return string
 
     @pyqtSlot(str, str)
     def edit_tags(self, tuple_pair_id, tags):
-        ApplicationContext.COMPLETE_DATA_FRAME.loc[ApplicationContext.COMPLETE_DATA_FRAME['_id'] == int(tuple_pair_id), ApplicationContext.TAGS_COLUMN] = tags
+        ApplicationContext.COMPLETE_DATA_FRAME.loc[
+            ApplicationContext.COMPLETE_DATA_FRAME['_id'] == int(tuple_pair_id), ApplicationContext.TAGS_COLUMN] = tags
 
     @pyqtSlot(str, str)
     def edit_comments(self, tuple_pair_id, comments):
