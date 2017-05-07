053b49445294a7a155affeded59921a23206c287
pavankm <pavank@protonmail.com>
2017-05-07 12:49:33 -0500

    refactoring StatsController

--- controller/StatsController.py
+++ controller/StatsController.py
@@ -1,62 +1,74 @@
-from PyQt5.QtCore import QObject, pyqtSlot
+from PyQt5.QtCore import QObject
 
 from utils import ApplicationContext
 
 
 class StatsController(QObject):
+    """
+    Computes statistics to be displayed
+    """
+
     def __init__(self, main_page):
         super(StatsController, self).__init__(None)
         self.main_page = main_page
 
-    @pyqtSlot()
-    # todo 5/4/17 if df = application context current_data will that keep up with changes?
     def count_matched_tuple_pairs(self, data_frame):
-        """ Returns a count of tuple pairs whose label value is MATCH
+        """ Returns a count of tuple pairs whose label value is MATCH.
 
-        :param data_frame:
-        :return:
+        Args: 
+            data_frame (DataFrame): Pandas data frame with label column.
+        
+        Returns:
+            Count of tuple pairs with label == MATCH (int).
+
+        Raises:
+                        
         """
         # todo check if assertion is correct thing to do
         assert ('label' in data_frame.columns)
-        # todo check data type of label column
         return data_frame[data_frame.label == ApplicationContext.MATCH].shape[0]
 
-    @pyqtSlot()
     def count_non_matched_tuple_pairs(self, data_frame):
         """Returns a count of tuple pairs whose label value is NON-MATCH
 
-        :param data_frame:
-        :return:
+        Args: 
+            data_frame (DataFrame): Pandas data frame with label column.
+        
+        Returns:
+            Count of tuple pairs with label == NON MATCH (int).
+
+        Raises:
         """
         # todo check if assertion is correct thing to do
         assert ('label' in data_frame.columns)
-        # todo check data type of label column
         return data_frame[data_frame.label == ApplicationContext.NON_MATCH].shape[0]
 
-    @pyqtSlot()
     def count_not_labeled_tuple_pairs(self, data_frame):
         """Returns a count of tuple pairs whose label value is NOT_LABELED
 
-        :param data_frame:
-        :return:
+        Args: 
+            data_frame (DataFrame): Pandas data frame with label column.
+        
+        Returns:
+            Count of tuple pairs with label == NOT LABELED (int).
+
+        Raises:
         """
         # todo check if assertion is correct thing to do
         assert ('label' in data_frame.columns)
-        # todo check data type of label column
         return data_frame[data_frame.label == ApplicationContext.NOT_LABELED].shape[0]
 
-    @pyqtSlot()
     def count_not_sure_tuple_pairs(self, data_frame):
         """Returns a count of tuple pairs whose label value is NOT_SURE
 
-        :param data_frame:
-        :return:
+        Args: 
+            data_frame (DataFrame): Pandas data frame with label column.
+        
+        Returns:
+            Count of tuple pairs with label == NOT SURE (int).
+
+        Raises:
         """
         # todo check if assertion is correct thing to do
         assert ('label' in data_frame.columns)
-        # todo check data type of label column
         return data_frame[data_frame.label == ApplicationContext.NOT_SURE].shape[0]
-
-    @pyqtSlot()
-    def count_tuple_pairs(self, data_frame):
-        return data_frame.shape[0]
